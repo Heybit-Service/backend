10bd7696277b2e8c009682690b0fa940c9d95e8b
jueun275 <52851721+jueun275@users.noreply.github.com>
2025-07-07 15:13:14 +0900

    Initial commit

---
+++ src/main/java/com/heybit/backend/HeybitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.heybit.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeybitBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HeybitBackendApplication.class, args);
+  }
+
+}
