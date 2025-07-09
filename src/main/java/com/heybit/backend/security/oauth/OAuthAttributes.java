2aff1324e235e9c77609fb8486201a88113efcd7
jueun275 <52851721+jueun275@users.noreply.github.com>
2025-07-09 17:46:49 +0900

    feat: 소셜 로그인 기능 구현

---
+++ src/main/java/com/heybit/backend/security/oauth/OAuthAttributes.java
@@ -0,0 +1,70 @@
+package com.heybit.backend.security.oauth;
+
+
+import com.heybit.backend.domain.user.Role;
+import com.heybit.backend.domain.user.UserEntity;
+import com.heybit.backend.domain.user.UserStatus;
+import java.util.Map;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class OAuthAttributes {
+
+  private Map<String, Object> attributes;
+  private String nameAttributeKey;
+  private String nickname;
+  private String email;
+  private String provider;
+
+  @Builder
+  public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String nickname,
+      String email, String provider) {
+    this.attributes = attributes;
+    this.nameAttributeKey = nameAttributeKey;
+    this.nickname = nickname;
+    this.email = email;
+    this.provider = provider;
+  }
+
+  public static OAuthAttributes of(String registrationId, String userNameAttributeName,
+      Map<String, Object> attributes) {
+    if ("kakao".equals(registrationId)) {
+      return ofKakao(userNameAttributeName, attributes, registrationId);
+    }
+
+    return ofGoogle(userNameAttributeName, attributes, registrationId);
+  }
+
+  private static OAuthAttributes ofGoogle(String userNameAttributeName,
+      Map<String, Object> attributes, String provider) {
+    return OAuthAttributes.builder()
+        .email((String) attributes.get("email"))
+        .provider(provider)
+        .attributes(attributes)
+        .nameAttributeKey(userNameAttributeName)
+        .build();
+  }
+
+  private static OAuthAttributes ofKakao(String userNameAttributeName,
+      Map<String, Object> attributes, String provider) {
+    Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+
+    return OAuthAttributes.builder()
+        .email((String) kakaoAccount.get("email"))
+        .provider(provider)
+        .attributes(attributes)
+        .nameAttributeKey(userNameAttributeName)
+        .build();
+  }
+
+  public UserEntity toEntity() {
+    return UserEntity.builder()
+        .email(email)
+        .nickname(null)
+        .role(Role.USER)
+        .status(UserStatus.ACTIVE)
+        .provider(provider)
+        .build();
+  }
+}
