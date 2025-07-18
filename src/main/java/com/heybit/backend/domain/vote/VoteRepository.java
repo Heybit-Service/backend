20fd11ecaabac9055ea650b9605c5402b7a60219
jueun275 <52851721+jueun275@users.noreply.github.com>
2025-07-18 17:52:49 +0900

    feat: 투표 글에 대한 투표 및 투표 취소 기능 구현

--- src/main/java/com/heybit/backend/domain/vote/VoteRepository.java
+++ src/main/java/com/heybit/backend/domain/vote/VoteRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 
+  boolean existsByUserIdAndProductVotePostId(Long userId, Long postId);
+
+  void deleteByUserIdAndProductVotePostId(Long userId, Long postId);
 }
