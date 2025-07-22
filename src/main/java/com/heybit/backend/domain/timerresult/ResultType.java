61f4717ac0a4dd577ec18068b3a3ef5932cff304
jueun275 <52851721+jueun275@users.noreply.github.com>
2025-07-22 17:57:14 +0900

    feat: TimerResult entity, repository 추가

---
+++ src/main/java/com/heybit/backend/domain/timerresult/ResultType.java
@@ -0,0 +1,5 @@
+package com.heybit.backend.domain.timerresult;
+
+public enum ResultType {
+  PURCHASED, SAVED
+}
---
+++ src/main/java/com/heybit/backend/domain/timerresult/TimerResultRepository.java
@@ -0,0 +1,7 @@
+package com.heybit.backend.domain.timerresult;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimerResultRepository extends JpaRepository<TimerResult, Long> {
+
+}
