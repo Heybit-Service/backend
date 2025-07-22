86dd6ce4f67240692af4e66fd16a4bab07f63976
jueun275 <52851721+jueun275@users.noreply.github.com>
2025-07-23 05:19:26 +0900

    test: 유저의 완료된 타이머 결과 조회 테스트 추가
    - 유저가 등록한 COMPLETED, ABANDONED 상태의 타이머 결과 조회 테스트
    - 절제 성공(SAVED), 실패(PURCHASED, ABANDONED)에 따른 메시지 포맷 검증

--- src/test/java/com/heybit/backend/application/service/ProductTimerResultServiceTest.java
+++ src/test/java/com/heybit/backend/application/service/ProductTimerResultServiceTest.java
@@ -1,7 +1,8 @@
 package com.heybit.backend.application.service;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 import com.heybit.backend.domain.productinfo.Category;
 import com.heybit.backend.domain.productinfo.ProductInfo;
@@ -16,10 +17,15 @@ import com.heybit.backend.domain.user.Role;
 import com.heybit.backend.domain.user.User;
 import com.heybit.backend.domain.user.UserRepository;
 import com.heybit.backend.domain.user.UserStatus;
+import com.heybit.backend.domain.votepost.ProductVotePost;
 import com.heybit.backend.global.exception.ApiException;
 import com.heybit.backend.global.exception.ErrorCode;
+import com.heybit.backend.presentation.timer.dto.CompletedTimerResponse;
 import com.heybit.backend.presentation.timerresult.dto.TimerResultRequest;
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Timer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -40,40 +46,59 @@ class ProductTimerResultServiceTest {
   @Autowired
   private TimerResultRepository timerResultRepository;
 
-  private ProductTimer timer;
   @Autowired
   private UserRepository userRepository;
+
   @Autowired
   private ProductInfoRepository productInfoRepository;
 
+  private User user;
+
   @BeforeEach
   void setUp() {
-    User user = userRepository.save(User.builder()
+    user = userRepository.save(User.builder()
         .nickname("jun")
         .email("jun@example.com")
         .role(Role.USER)
         .status(UserStatus.ACTIVE)
         .build());
+  }
 
-    ProductInfo info = productInfoRepository.save(ProductInfo.builder()
-        .name("에어팟")
-        .amount(10000)
-        .category(Category.ETC)
-        .build());
+  private ProductTimer createTimer(
+      String name,
+      int amount,
+      LocalDateTime start,
+      LocalDateTime endTime
+  ) {
+    var productInfo = productInfoRepository.save(
+        ProductInfo.builder()
+            .name(name)
+            .amount(amount)
+            .category(Category.ETC)
+            .build());
 
-    timer = productTimerRepository.save(ProductTimer.builder()
-        .startTime(LocalDateTime.now().minusHours(1))
-        .endTime(LocalDateTime.now().plusHours(1))
-        .status(TimerStatus.IN_PROGRESS)
-        .user(user)
-        .productInfo(info)
-        .build());
+    var timer = productTimerRepository.save(
+        ProductTimer.builder()
+            .startTime(start)
+            .endTime(endTime)
+            .status(TimerStatus.IN_PROGRESS)
+            .productInfo(productInfo)
+            .user(user)
+            .build());
+
+    return timer;
   }
 
   @Test
   @DisplayName("타이머 결과 저장 및 타이머 상태 변경 테스트")
   void saveResult_successfully() {
     //given
+    ProductTimer timer = createTimer("에어팟",
+        3000,
+        LocalDateTime.now().minusHours(1),
+        LocalDateTime.now().plusHours(1)
+    );
+
     TimerResultRequest request = TimerResultRequest.builder()
         .productTimerId(timer.getId())
         .result(ResultType.SAVED)
@@ -99,6 +124,12 @@ class ProductTimerResultServiceTest {
   @DisplayName("이미 결과가 등록된 타이머에 결과 등록 시도 시 예외가 발생")
   void saveResult_AlreadyRegistered() {
     // given
+    ProductTimer timer = createTimer("에어팟",
+        3000,
+        LocalDateTime.now().minusHours(1),
+        LocalDateTime.now().plusHours(1)
+    );
+
     TimerResultRequest request = TimerResultRequest.builder()
         .productTimerId(timer.getId())
         .result(ResultType.SAVED)
@@ -111,6 +142,61 @@ class ProductTimerResultServiceTest {
     assertThatThrownBy(() -> timerResultService.registerResult(request))
         .isInstanceOf(ApiException.class)
         .hasMessageContaining(ErrorCode.ALREADY_REGISTERED_RESULT.getMessage());
+  }
+
+  @DisplayName("완료된 타이머에 대한 정보를 조회 한다")
+  @Test
+  void getCompletedResultsByUser_success() {
+    // given
+    ProductTimer completedTimer = createTimer("성공타이머",
+        3000,
+        LocalDateTime.now().minusHours(12),
+        LocalDateTime.now().minusMinutes(30)
+    );
+
+    ProductTimer abandonedTimer = createTimer("포기타이머",
+        6000,
+        LocalDateTime.now().minusMinutes(450),
+        LocalDateTime.now().minusMinutes(1)
+    );
+
+    timerResultRepository.save(
+        TimerResult.builder()
+            .productTimer(completedTimer)
+            .result(ResultType.SAVED)
+            .savedAmount(completedTimer.getProductInfo().getAmount())
+            .build()
+    );
+
+    timerResultRepository.save(
+        TimerResult.builder()
+            .productTimer(abandonedTimer)
+            .result(ResultType.PURCHASED)
+            .consumedAmount(abandonedTimer.getProductInfo().getAmount())
+            .build()
+    );
+
+    completedTimer.updateState(TimerStatus.COMPLETED);
+    abandonedTimer.updateState(TimerStatus.ABANDONED);
+
+    // when
+    List<CompletedTimerResponse> responses = timerResultService.getCompletedResultsByUserId(
+        user.getId());
+
+    System.out.println(responses);
+
+    assertThat(responses)
+        .hasSize(2)
+        .extracting(
+            CompletedTimerResponse::getName,
+            CompletedTimerResponse::isSuccess
+        )
+        .containsExactlyInAnyOrder(
+            tuple("성공타이머", true),
+            tuple("포기타이머", false)
+        );
+
+
   }
 }
 
