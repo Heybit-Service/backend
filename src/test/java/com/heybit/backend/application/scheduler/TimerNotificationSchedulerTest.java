3a45194288ce8f5e1eecc223826ef72eb7943a22
jueun275 <52851721+jueun275@users.noreply.github.com>
2025-07-25 16:50:05 +0900

    test: 타이머 삭제 시 알림 스케줄 제거 테스트 추가
    - 타이머 삭제 로직 호출 시 등록된 Quartz Job이 정상적으로 삭제되는지 검증

--- src/test/java/com/heybit/backend/application/scheduler/TimerNotificationSchedulerTest.java
+++ src/test/java/com/heybit/backend/application/scheduler/TimerNotificationSchedulerTest.java
@@ -1,18 +1,16 @@
 package com.heybit.backend.application.scheduler;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.heybit.backend.infrastructure.quartz.NotificationJobSchedulerFactory;
 import java.time.LocalDateTime;
-import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.quartz.JobDetail;
 import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
-import org.quartz.impl.matchers.GroupMatcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -28,8 +26,10 @@ public class TimerNotificationSchedulerTest {
   @Autowired
   private Scheduler scheduler;
 
-  private final Long testTimerId = 999L;
-  private final String testName = "Test Timer";
+  @BeforeEach
+  void setUp() throws SchedulerException {
+    scheduler.clear();
+  }
 
   @Test
   @DisplayName("30분 타이머 등록시 알림 스케줄링 예약 검증")
@@ -46,5 +46,30 @@ public class TimerNotificationSchedulerTest {
     // then
     assertTrue(scheduler.checkExists(JobKey.jobKey("NotificationJob_second_quarter", "TIMER_1")));
     assertTrue(scheduler.checkExists(JobKey.jobKey("NotificationJob_completed", "TIMER_1")));
+  }
+
+  @Test
+  @DisplayName("타이머가 삭제될떄 해당 id의 모든 스케줄 삭제 검증")
+  void cancelAllByTimerId() throws Exception {
+    // given
+    LocalDateTime now = LocalDateTime.now();
+    LocalDateTime end = now.plusMinutes(60);
+    Long timerId = 2L;
+    String name = "ToDeleteTimer";
+
+    notificationScheduler.scheduleTimerNotificationJob(now, end, timerId, name);
+
+    // 사전 확인
+    assertTrue(scheduler.checkExists(JobKey.jobKey("NotificationJob_second_quarter", "TIMER_2")));
+    assertTrue(scheduler.checkExists(JobKey.jobKey("NotificationJob_nearly_done", "TIMER_2")));
+    assertTrue(scheduler.checkExists(JobKey.jobKey("NotificationJob_completed", "TIMER_2")));
+
+    // when
+    notificationScheduler.cancelTimerNotificationJob(timerId);
+
+    // then
+    assertFalse(scheduler.checkExists(JobKey.jobKey("NotificationJob_second_quarter", "TIMER_2")));
+    assertFalse(scheduler.checkExists(JobKey.jobKey("NotificationJob_nearly_done", "TIMER_2")));
+    assertFalse(scheduler.checkExists(JobKey.jobKey("NotificationJob_completed", "TIMER_2")));
   }
 }
