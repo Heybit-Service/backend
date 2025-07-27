9d5aa4e0e06fe20cb4b96bd4f21ae6a19271a941
jueun275 <52851721+jueun275@users.noreply.github.com>
2025-07-28 04:08:36 +0900

    feat: S3 이미지 업로드 컴포넌트 구현
    - S3 업로드 컴포넌트 추가 및 로깅 적용
    - 타이머 생성 테스트 시 이미지 파일 대신 null 처리로 테스트 단순화
    - S3 업로드는 postman 테스트 완료

--- src/main/java/com/heybit/backend/infrastructure/s3/S3UploadComponent.java
+++ src/main/java/com/heybit/backend/infrastructure/s3/S3UploadComponent.java
@@ -1,24 +1,66 @@
 package com.heybit.backend.infrastructure.s3;
 
 import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class S3UploadComponent {
 
-  public String upload(MultipartFile multipartFile, Long userCd) throws IOException {
+  private static final String PRODUCT_DIR = "product-images";
 
-    // TODO: S3 업로드 로직 직성
-    log.info("upload file");
+  private final S3Client s3Client;
 
-    return null;
+  @Value("${cloud.aws.s3.bucket}")
+  private String bucket;
+
+  public String upload(MultipartFile file, Long userId) throws IOException {
+
+    log.info("Start upload file");
+
+    String key = buildFileKey(userId, file);
+
+    PutObjectRequest request = PutObjectRequest.builder()
+        .bucket(bucket)
+        .key(key)
+        .contentType(file.getContentType())
+        .build();
+
+    s3Client.putObject(request, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+    log.info("Successfully uploaded file: key={}", key);
+    return generateUrl(key);
+  }
+
+  private String buildFileKey(Long userId, MultipartFile file) {
+    String originalFilename = Optional.ofNullable(file.getOriginalFilename()).orElse("file");
+    String ext = "";
+
+    int idx = originalFilename.lastIndexOf(".");
+    if (idx != -1) {
+      ext = originalFilename.substring(idx);
+    }
+
+    String uuid = UUID.randomUUID().toString();
+
+    return String.format("%d/%s/product-%s%s", userId, PRODUCT_DIR, uuid, ext.toLowerCase());
+  }
+
+  private String generateUrl(String key) {
+    return "https://" + bucket + ".s3.amazonaws.com/" + key;
   }
 
   public void delete(String imageUrl) {
-
     // TODO: S3 업로드 삭제 로직 직성
     log.info("delete file");
   }
