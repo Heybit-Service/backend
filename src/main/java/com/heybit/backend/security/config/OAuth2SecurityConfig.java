387ab514e0c8d5b25ce004ab854b6b3e626a2ca0
Seungsu Yoo <zenorate@gmail.com>
2025-08-25 02:05:56 +0900

    feat: OAuth2 설정에 CustomAuthorizationRequestResolver 적용

--- src/main/java/com/heybit/backend/security/config/OAuth2SecurityConfig.java
+++ src/main/java/com/heybit/backend/security/config/OAuth2SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.heybit.backend.security.config;
 
+import com.heybit.backend.security.oauth.CustomAuthorizationRequestResolver;
 import com.heybit.backend.security.oauth.CustomOAuth2UserService;
 import com.heybit.backend.security.oauth.OAuth2LoginFailureHandler;
 import com.heybit.backend.security.oauth.OAuth2LoginSuccessHandler;
@@ -16,9 +17,13 @@ public class OAuth2SecurityConfig {
   private final CustomOAuth2UserService customOAuth2UserService;
   private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
   private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
+  private final CustomAuthorizationRequestResolver customAuthorizationRequestResolver;
 
   public void configure(HttpSecurity http) throws Exception {
     http.oauth2Login(oauth2 -> oauth2
+        .authorizationEndpoint(authorization -> authorization
+            .authorizationRequestResolver(customAuthorizationRequestResolver)
+        )
         .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
         .successHandler(oAuth2LoginSuccessHandler)
         .failureHandler(oAuth2LoginFailureHandler)
