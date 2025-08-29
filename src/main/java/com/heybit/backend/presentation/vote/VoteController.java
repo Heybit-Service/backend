de9ced4e64cfca2bfb79b3cefa05e83c78987092
jueun275 <52851721+jueun275@users.noreply.github.com>
2025-08-29 17:25:56 +0900

    feat: 내가 투표한투표 정보조회  controller api 등록

--- src/main/java/com/heybit/backend/presentation/vote/VoteController.java
+++ src/main/java/com/heybit/backend/presentation/vote/VoteController.java
@@ -4,10 +4,12 @@ package com.heybit.backend.presentation.vote;
 import com.heybit.backend.application.service.VoteService;
 import com.heybit.backend.domain.vote.VoteResultType;
 import com.heybit.backend.global.response.ApiResponseEntity;
+import com.heybit.backend.presentation.vote.dto.VotedPostResponse;
 import com.heybit.backend.security.oauth.LoginUser;
-import java.time.temporal.ValueRange;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class VoteController {
 
   private final VoteService voteService;
+
+  @GetMapping("/my-votes")
+  public ApiResponseEntity<List<VotedPostResponse>> getMyVotedPosts(@LoginUser Long userId) {
+    List<VotedPostResponse> votedPosts = voteService.getMyVotedPosts(userId);
+    return ApiResponseEntity.success(votedPosts);
+  }
 
   @PostMapping("/{votePostId}/vote")
   public ApiResponseEntity<Void> vote(
