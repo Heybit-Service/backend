973d57bb46cd89fa6250eadf03277df44618c177
jueun275 <52851721+jueun275@users.noreply.github.com>
2025-09-08 18:36:33 +0900

    feat: Quartz DB 모드에서만 DataSource 적용

--- src/main/java/com/heybit/backend/infrastructure/quartz/QuartzConfig.java
+++ src/main/java/com/heybit/backend/infrastructure/quartz/QuartzConfig.java
@@ -1,8 +1,10 @@
 package com.heybit.backend.infrastructure.quartz;
 
+import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,11 +15,22 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 public class QuartzConfig {
 
   private final AutowireCapableBeanFactory beanFactory;
+  private final DataSource dataSource;
+
+  @Value("${spring.quartz.job-store-type}")
+  private String jobStoreType;
 
   @Bean
   public SchedulerFactoryBean schedulerFactoryBean(AutowiringSpringBeanJobFactory jobFactory) {
     SchedulerFactoryBean factory = new SchedulerFactoryBean();
     factory.setJobFactory(jobFactory);
+    factory.setOverwriteExistingJobs(false);
+    factory.setWaitForJobsToCompleteOnShutdown(true);
+    
+    if ("jdbc".equalsIgnoreCase(jobStoreType)) {
+      factory.setDataSource(dataSource);
+    }
+
     return factory;
   }
 
