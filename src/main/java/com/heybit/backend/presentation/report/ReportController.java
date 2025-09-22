236b97d0aca9bf7fb3719410426cd68239f152db
jueun275 <52851721+jueun275@users.noreply.github.com>
2025-09-22 10:51:29 +0900

    fix: ReportController에 @RestController 추가 및 요청 경로 수정

--- src/main/java/com/heybit/backend/presentation/report/ReportController.java
+++ src/main/java/com/heybit/backend/presentation/report/ReportController.java
@@ -11,10 +11,12 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
+@RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/reports")
+@RequestMapping("/api/v1/reports")
 public class ReportController {
 
   private final ReportService reportService;
